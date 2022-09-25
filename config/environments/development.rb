348d985153d979e341ea2073bc98d313bfe73644
Miller <miller@daangn.com>
2022-09-26 00:13:48 +0900

    Add Redis

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,16 +23,20 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
   end
 
+  config.cache_store = :redis_store, {
+    host: "127.0.0.1",
+    port: 6379,
+    db: 0,
+  }
+
+  config.active_record.cache_versioning = false
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
