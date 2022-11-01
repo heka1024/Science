5d05a829fa865950c02d4686c00a38638640729e
Miller <miller@daangn.com>
2022-11-01 23:52:34 +0900

    Use turbo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  attr_reader :page
+
   def index
-    page = params[:page].to_i || 1
+    @page = params[:page].to_i || 1
     @posts = Post.all.page(page)
-    # @next_page = page + 1 if (Post.all.count > 10 * page + 10)
-    @next_page = nil
+    puts @posts
+    @next_page = page + 1 if (Post.all.count > 10 * page + 10)
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
